db519fe33c57d349e96dc545f2b7c0c7eefd0f3a
Adarsh Dhar <dharadarsh0@gmail.com>
2024-10-31 20:00:47 +0530

    contract built

--- hello-world/tests/hello-world.ts
+++ hello-world/tests/hello-world.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { HelloWorld } from "../target/types/hello_world";
+import assert from "assert";
+
 
 describe("hello-world", () => {
   // Configure the client to use the local cluster.
@@ -13,4 +15,28 @@ describe("hello-world", () => {
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Can create a message", async () => {
+    const message = anchor.web3.Keypair.generate();
+    const messageContent = "Hello, World!";
+    await program.rpc.createMessage(messageContent, {
+      accounts : {
+        message : message.publicKey,
+        author : anchor.web3.SystemProgram.programId,
+        systemProgram : anchor.web3.SystemProgram.programId,
+
+      },
+      signers : [message],
+
+    });
+
+
+    const messageAccount = await program.account.message.fetch(message.publicKey);
+    
+
+    assert.equal(
+      messageAccount.author.toBase58(),
+      provider.wallet.publicKey.toBase58()
+    )
+  })
 });
